a3fb1ffa61ee29ab9bec33121da53ab13e170f49
Pratiksha Chavan <pratikshakchavan123@gmail.com>
2025-11-03 10:34:12 +0530

    Add files via upload

---
+++ Assignments/Assignment_2/program2_1.c
@@ -0,0 +1,26 @@
+// 1. Accept number from user and print number of * on screen.
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+    
+    while(iCnt < iNo)
+    {
+        printf("* ");
+        iCnt++;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number\n");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
---
+++ Assignments/Assignment_2/program2_5.c
@@ -0,0 +1,39 @@
+// 5. Accept number from user and check whether number is even or odd.
+
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckEvenOdd(int iNo)
+{
+    if((iNo % 2) == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter number\n");
+    scanf("%d", &iValue);
+
+    bRet = CheckEvenOdd(iValue);
+
+    if(bRet == true)
+    {
+        printf("%d is even number\n",iValue);
+    }
+    else
+    {
+        printf("%d is odd number\n",iValue);
+    }
+
+    return 0;
+}
